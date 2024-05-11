a5d8d1af1a6b26e7e52567841ed295f870030058
shillo <kimsemen0402@mail.ru>
2024-05-11 15:47:14 +0300

    Add error handling using try/catch

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
 import "./style.css";
 import { moveLeft, moveRight } from "./slider.js";
+import { getCurrentWeather } from "./getCurrentWeather.js";
+import { getHourWeather } from "./getHourWeather.js";
 
 getCurrentWeather("Moscow");
 getHourWeather("Moscow");
-
-async function getCurrentWeather(place) {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=e9c674b0342042a48b0160004240505&q=${place}`,
-    { mode: "cors" }
-  );
-  const data = await response.json();
-  let city = document.querySelector("#city");
-  let temp_c = document.querySelector("#temperature").children[0];
-  let humidity = document.querySelector("#humidity").children[1];
-  let windSpeed = document.querySelector("#windSpeed").children[1];
-  let pressure = document.querySelector("#pressure").children[1];
-
-  city.textContent = data.location.name;
-  temp_c.textContent = data.current.temp_c + "°C";
-  humidity.textContent = data.current.humidity;
-  windSpeed.textContent = data.current.wind_kph;
-  pressure.textContent = data.current.pressure_mb;
-
-  document.querySelector("input").value = "";
-
-  let img = document.querySelector("#icon");
-  img.src = data.current.condition.icon;
-}
 
 document.querySelector("button").addEventListener("click", function () {
   if (document.querySelector("input").value) {
@@ -50,38 +28,6 @@ document.addEventListener("keyup", (event) => {
   }
 });
 
-async function getHourWeather(place) {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/forecast.json?key=e9c674b0342042a48b0160004240505&q=${place}&days=1`,
-    { mode: "cors" }
-  );
-  const data = await response.json();
-
-  let container = document.querySelector("#container");
-  container.innerHTML = "";
-  let array = data.forecast.forecastday[0].hour;
-  for (let i = 0; i <= array.length; i++) {
-    let card = document.createElement("div");
-    card.setAttribute("class", "card");
-
-    let time = document.createElement("p");
-    time.setAttribute("class", "time");
-    time.textContent = array[i].time.slice(-5);
-    card.appendChild(time);
-
-    let img = document.createElement("img");
-    img.src = array[i].condition.icon;
-    card.appendChild(img);
-
-    let hourTemp = document.createElement("p");
-    hourTemp.setAttribute("class", "hourTemp");
-    hourTemp.textContent = array[i].temp_c + "°C";
-    card.appendChild(hourTemp);
-
-    container.appendChild(card);
-  }
-}
-
 document.querySelector("#left").addEventListener("click", function () {
   moveLeft();
 });
@@ -89,5 +35,3 @@ document.querySelector("#left").addEventListener("click", function () {
 document.querySelector("#right").addEventListener("click", function () {
   moveRight();
 });
-
-getHourWeather("Moscow");
